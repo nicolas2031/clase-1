c868e8917911422139bb5e5c1f82cb9457d99230
USUARIO <USUARIO@DESKTOP-K6KE95E>
2020-04-08 12:05:37 -0500

    cambio

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+
+
+int main()
+
+{
+
+	int x = 10;
+	int y = 5;
+	int z = 2;
+
+
+	int suma = x + y;
+	int resta = x - y;
+	int multip = x * y;
+
+
+	std::cout << "el resultado de la suma es:" << suma << std::endl;
+	std::cout << "el resultado de la resta es:" << resta << std::endl;
+	std::cout << "el resultado de la multiplicacion es:" << multip << std::endl;
+	return 0;
+}
